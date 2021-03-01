a8d581fd7ca0dcde04a46d803a8db9416a7687af
example <77124693+kayzanburg@users.noreply.github.com>
2021-03-01 23:25:42 +0300

    Yildiz Ile K Yapimi

---
+++ Yildiz_Ile_K_yapimi/Yildiz_Ile_K_yapimi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yildiz_Ile_K_yapimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0 ; i <= 13 ; i++)
+            {
+
+                for (int t = 13; i <= t; t--)
+                {
+                    Console.Write("*");
+                }
+                
+                Console.Write("\n");
+            }
+
+            for (int i = 0; i <= 13; i++)
+            {
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
